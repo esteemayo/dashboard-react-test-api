725ddfaa6b6abc05b0b11c4526792830164d6c88
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-21 22:21:51 +0100

    feat: handle get product by id api logic

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,3 +5,13 @@ import { products } from '../dev-data/data/products.js';
 export const getProducts = (req, res, next) => {
   return res.status(StatusCodes.OK).json(products);
 };
+
+export const getProduct = (req, res, next) => {
+  const { id: productId } = req.params;
+
+  const product = products.find(
+    (product) => product.id === parseInt(productId)
+  );
+
+  return res.status(StatusCodes.OK).json(product);
+};
