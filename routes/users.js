ef39ece7bb74ca11029d3314db8be0806766aa25
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-21 22:12:38 +0100

    feat: add delete user api route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,15 @@
 import express from 'express';
 
-import { createUser, getUser } from '../controllers/userController.js';
+import {
+  createUser,
+  deleteUser,
+  getUser,
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.route('/').post(createUser);
 
-router.route('/:id').get(getUser);
+router.route('/:id').get(getUser).delete(deleteUser);
 
 export default router;
