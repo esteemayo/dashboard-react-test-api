86eacd3111e48b6a2d3d198c9058a5f9b0777650
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-21 22:52:55 +0100

    fix: add middlewares

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import compression from 'compression';
 import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
 import 'colors';
 
 import userRoute from './routes/users.js';
@@ -11,11 +13,21 @@ const app = express();
 app.use(cors());
 app.options('*', cors());
 
-app.use(express.json({ limit: '10kb' }));
-
 if (app.get('env') === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 30 * 60 * 1000,
+  message: 'Too many request from this IP, Please try again in 30 minutes',
+});
+
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use(compression());
 
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/products', productRoute);
