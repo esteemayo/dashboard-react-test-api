4ec9609a181a579c05be03fa7ed8afa0641eaf00
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-21 22:06:36 +0100

    feat: add get user by id route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import { getUser } from '../controllers/userController.js';
+
 const router = express.Router();
+
+router.route('/:id').get(getUser);
 
 export default router;
