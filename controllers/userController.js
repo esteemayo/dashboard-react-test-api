f5485df3aff101a1fad0702c1a6b465687af3bba
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-21 22:12:56 +0100

    fix: modify

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 
 import { users } from '../dev-data/data/users.js';
 
-export const getUser = async (req, res, next) => {
+export const getUser = (req, res, next) => {
   const { id: userId } = req.params;
 
   const user = users.find((user) => user.id === parseInt(userId));
