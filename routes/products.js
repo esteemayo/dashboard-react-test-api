08d3ea0310808139c2d0a3b4c5675d5983a54a56
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-21 22:19:40 +0100

    feat: add fetch all products api route

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import { getProducts } from '../controllers/productController';
+
 const router = express.Router();
+
+router.route('/').get(getProducts);
 
 export default router;
