324e8cb0eaf2bbae95a2f9d892bf3640fe720555
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-21 16:35:10 +0100

    feat: add routes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
+import userRoute from './routes/users';
+import productRoute from './routes/products';
+
 const app = express();
 
 app.use(cors());
@@ -10,5 +13,8 @@ app.options('*', cors());
 if (app.get('env') === 'development') {
   app.use(morgan('dev'));
 }
+
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/products', productRoute);
 
 export default app;
