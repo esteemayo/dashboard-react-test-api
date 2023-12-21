72ddc349d1e30ebdff53fbe4437f5038ce9176e7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-21 22:09:01 +0100

    feat: handle create new user api logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,3 +8,9 @@ export const getUser = async (req, res, next) => {
   const user = users.find((user) => user.id === parseInt(userId));
   return res.status(StatusCodes.OK).json(user);
 };
+
+export const createUser = (req, res, next) => {
+  const user = users.push({ ...req.body });
+
+  return res.status(StatusCodes.CREATED).json(user);
+};
