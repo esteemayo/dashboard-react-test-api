c400484aa149ebdc63c3496af06108c420c18702
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-21 16:08:36 +0100

    feat: setup express server

---
+++ server.js
@@ -0,0 +1,9 @@
+import 'colors';
+
+import app from './app.js';
+
+app.set('port', process.env.PORT || 8800);
+
+const server = app.listen(app.get('port'), () =>
+  console.log(`App listening on port ↔ ${server.address().port}`.cyan.bold)
+);
