797111770aa6862f3dbf7a4863c677433934f733
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-22 02:46:24 +0100

    fix: modify

--- dev-data/data/users.js
+++ dev-data/data/users.js
@@ -1,4 +1,4 @@
-export const users = [
+export let users = [
   {
     id: 1,
     img: 'https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
